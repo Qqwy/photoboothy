7f86ce9697278b94a6ca446f4aa89940d5a21ba5
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-05-08 06:54:43 +0200

    fix config?

--- fw/config/config.exs
+++ fw/config/config.exs
@@ -87,4 +87,4 @@ config :nerves_firmware_ssh,
 # of this file so it overrides the configuration defined above.
 # Uncomment to use target specific configurations
 
-# import_config "#{Mix.Project.config[:target]}.exs"
+import_config "#{Mix.Project.config[:target]}.exs"
